e868b6bf7eb590b1d07de384e526c2d17b821a71
Taranvir <thidkyar@gmail.com>
2018-06-22 16:15:03 +0000

    added admin security

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']  
 
   def show
     @category = Category.find(params[:id])
