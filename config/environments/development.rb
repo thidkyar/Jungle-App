c5f82bea6b6ab492cc14ea8fb88665eb913b38e1
Taranvir <thidkyar@gmail.com>
2018-06-22 14:25:08 +0000

    fixed email authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'jtl.lhl.test',
+    user_name:            'jt.lhl.test',
     password:             'tester123!',
     authentication:       'plain',
     enable_starttls_auto: true }
